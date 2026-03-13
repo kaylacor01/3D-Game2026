faeec0b9fd8ad93e2dce4f41b11bdf240c751f4b
Alondrag360 <alondrag4265@gmail.com>
2026-03-13 13:17:21 -0500

    Update BulletMovement.cs

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
+    //Speed of the bullet
+    public float bulletSpeed = 10f;
+
+    //Lifetime so bullets do not stay forever in the scene
+    public float bulletLifetime = 3f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Destroy the bullet automatically after a few seconds
+        Destroy(gameObject, bulletLifetime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move the bullet forward in the direction it is facing
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other){
+        //If the bullet touches an enemy, destroy the enemy
+        if(other.CompareTag("enemy")){
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
