67f56b4d3ad2da16bdcf4f2778a02089214c1122
Alondrag360 <alondrag4265@gmail.com>
2026-04-03 21:52:35 -0500

    Spawner Code

--- Assets/Scripts/SafeSpawner.cs
+++ Assets/Scripts/SafeSpawner.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class SafeSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject housePrefab;
+    public Transform player;
+    public float avoidRadius = 8f; //how far away from the player the house must spawn
+
+    //plane scale is 30,30,30 so half-size is 15
+    private float planeHalfsize = 15f;
+    
     void Start()
     {
-        
+         SpawnHouse();   
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnHouse()
     {
-        
+        Vector3 spawnPos;
+
+        //Keep picking a random point until it's OUTSIDE the avoid radius
+        do{
+            float x = Random.Range(-planeHalfSize, planeHalfSize);
+            float z = Random.Range(-planeHalfSize, planeHalfSize);
+
+            spawnPos = new Vector3(x, 0f, z);
+        }
+        while(Vector3.Distance(spawnPos, player.position) < avoid radius);
+
+        //spawn the house once 
+        Instantiate(housePrefab, spawnPos, Quaternion.identity);
     }
 }
